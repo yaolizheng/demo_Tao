42c7ba949dec8f58c11991b64fbc697d3bc8ea0d
TaoZong <soulllreaver@gmail.com>
2012-10-13 13:02:50 -0700

    Revert "Revert "from ZT""
    This reverts commit bf3a0c5279e7fa9a74b85917a97e712457dda99e.

---
+++ demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  demo
+//
+//  Created by Tao Zong on 10/10/12.
+//  Copyright (c) 2012 Tao Zong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "demoAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([demoAppDelegate class]));
+    }
+}
